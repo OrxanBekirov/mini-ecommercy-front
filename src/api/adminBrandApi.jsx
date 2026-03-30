317180d6efc6ee5d08b7eb1d13681cc7c6198657
OrxanBekirov <bekirov991@gmail.com>
2026-03-30 12:55:32 +0400

    update brand

--- src/api/adminBrandApi.jsx
+++ src/api/adminBrandApi.jsx
@@ -2,21 +2,21 @@ import axiosClient from "./axiosClient";
 
 
 export const getAllBrands = async () => {
-    const res = await axiosClient.get("/Brands");
+    const res = await axiosClient.get("/brands");
     return res.data.data || res.data
 }
 
 export const createBrandApi = async (data) => {
-    const res = await axiosClient.post("/Brands", data);
+    const res = await axiosClient.post("/brands", data);
     return res.data.data || res.data
 }
 
 export const deleteBrandApi = async (id) => {
-    const res = await axiosClient.delete(`/Brands/${id}`);
+    const res = await axiosClient.delete(`/brands/${id}`);
     return res.data.data || res.data
 }
 
 export const updateBrandApi = async (id, data) => {
-    const res = await axiosClient.put(`/Brands/${id}`, data);
+    const res = await axiosClient.put(`/brands/${id}`, data);
     return res.data.data || res.data
 }
